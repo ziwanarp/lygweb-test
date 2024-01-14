e4195b27b606e843b3ee75febf403fc23104230a
Ziwana Rizwan Pramudia <ziwanarizwan.pramudia@wingscorp.com>
2024-01-14 20:23:03 +0700

    final update, seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -335,6 +335,278 @@ class DatabaseSeeder extends Seeder
             'DestinationCode' => 'HK',
             'QtyOutput' => 32,
         ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'L',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 39,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XL',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 33,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XXL',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 11,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'S',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 9,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XL',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 30,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XXL',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 12,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'S',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 36,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'L',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 31,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XL',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 26,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XXL',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 13,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'L',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 6,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XL',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 11,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'BOSSE FANCY OH HOOD S.9',
+            'SizeName' => 'XXL',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 13,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '92',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 11,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '98',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 12,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '104',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 21,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '110',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 14,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '116',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 17,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '122',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 23,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '128',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 25,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '134',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 27,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '140',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 29,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '146',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 14,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'M. Zaidan',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '152',
+            'DestinationCode' => 'HK',
+            'QtyOutput' => 12,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '92',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 21,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '98',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 11,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '104',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 19,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '110',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 7,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '116',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 6,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '122',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 5,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '134',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 12,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '140',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 18,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '146',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 19,
+        ]);
+        \App\Models\LygSewingOutput::factory()->create([
+            'TrnDate' => '2024-01-03',
+            'OperatorName' => 'Afizza Shabira',
+            'StyleCode' => 'FOOTBALL SETS EUROCUP CW N (ARGENTINA) S.9',
+            'SizeName' => '152',
+            'DestinationCode' => 'SG',
+            'QtyOutput' => 21,
+        ]);
 
 
     }
